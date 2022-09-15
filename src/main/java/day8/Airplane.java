9305a87abde2e80c992a515c8852c36c2fa1cc28
FedorSvetlichnyy <svfedor84@gmail.com>
2022-09-15 22:14:51 +0300

    complete day8 tasks

---
+++ src/main/java/day8/Airplane.java
@@ -0,0 +1,62 @@
+package day8;
+
+class Airplane {
+    private String manufacturer;
+    private int year;
+    private double length;
+    private double weight;
+    private int fuel;
+
+    //сравниваем 2 самолета
+    public static void compareAirplanes(Airplane a, Airplane b){
+        if(a.getLength() > b.getLength())
+        System.out.println("ѕервый самолет длинее.");
+        else if (a.getLength() == b.getLength()){
+            System.out.println("ќни равны.");
+        }
+        else {
+            System.out.println("¬торой самолет длинее.");
+        }
+    }
+
+    public Airplane(String manufacturer, int year, double length, double weight){
+        this.manufacturer = manufacturer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+        this.fuel = 0;
+    }
+
+    public void setManufacturer(String manufacturer){
+        this.manufacturer = manufacturer;
+    }
+    public void setYear(int year){
+        this.year = year;
+    }
+    public void setLength(double length){
+        this.length = length;
+    }
+    public double getLength() {
+        return length;
+    }
+    public void setWeight(double weight){
+        this.weight = weight;
+    }
+    public void setFuel(int fuel){
+        this.fuel = fuel;
+    }
+    public int getFuel(){
+        return fuel;
+    }
+    void info(){
+        System.out.println("»зготовитель: " + manufacturer + ", год выпуска: " + year + ", длина: " + length + " м., вес: " + weight + " т., количество топлива в баке: " + fuel + " л.");
+    }
+    public int fillUp(int n){
+        this.fuel = fuel + n;
+        return fuel;
+    }
+    //следующий метод выводит информацию о самолете, аналогично вызову метода info().
+    public String toString(){
+        return "»зготовитель: " + manufacturer + ", год выпуска: " + year + ", длина: " + length + " м., вес: " + weight + " т., количество топлива в баке: " + fuel + " л.";
+    }
+}
--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,36 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        //1. числа через пробел  - цикл
+        long startTime = System.currentTimeMillis();
+        for(int i = 0; i<20001; i++){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        long stopTime = System.currentTimeMillis();
+        //System.out.println("Длительность работы 1 программы, в мс.: " + (stopTime-startTime));
+
+        //2. числа через пробел  - String
+        long startTime2 = System.currentTimeMillis();
+        for(int i = 0; i<20001; i++){
+            String string1 = String.format("%s ",i);
+            System.out.print(string1);
+        }
+        System.out.println();
+        long stopTime2 = System.currentTimeMillis();
+        //System.out.println("Длительность работы 2 программы, в мс.: " + (stopTime2-startTime2));
+
+        //3. числа через пробел  - StringBuilder
+        long startTime3 = System.currentTimeMillis();
+        for(int i = 0; i<20001; i++){
+            StringBuilder sb = new StringBuilder(i + " ");
+            System.out.print(sb);
+        }
+        System.out.println();
+        long stopTime3 = System.currentTimeMillis();
+        System.out.println("Длительность работы 1 программы, в мс.: " + (stopTime-startTime));
+        System.out.println("Длительность работы 2 программы, в мс.: " + (stopTime2-startTime2));
+        System.out.println("Длительность работы 3 программы, в мс.: " + (stopTime3-startTime3));
 
     }
 }
