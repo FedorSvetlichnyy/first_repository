ba386604a8053f117393535fa0c505e07beb9b2a
FedorSvetlichnyy <svfedor84@gmail.com>
2022-09-26 20:21:52 +0300

    complete day11 tasks

--- src/main/java/day11/task1/Task1.java
+++ src/main/java/day11/task1/Task1.java
@@ -1,7 +1,136 @@
 package day11.task1;
 
+import static day11.task1.Warehouse.*;
+
 public class Task1 {
     public static void main(String[] args) {
+        Warehouse warehouse1 = new Warehouse(); //создали склад 1
+        Picker picker1 = new Picker(warehouse1); //создали сборщика 1
+        Courier courier1 = new Courier(warehouse1); //создали курьера 1
+
+        businessProcess(picker1);
+        System.out.println(picker1);
+        System.out.println(warehouse1);
+        System.out.println(courier1);
+        businessProcess(courier1);
+        System.out.println(courier1);
+        System.out.println(warehouse1);
+
+        Warehouse warehouse2 = new Warehouse(); //создали склад 2
+        Courier courier2 = new Courier(warehouse2); //создали курьера 2
+        Picker picker2 = new Picker(warehouse2); //создали сборщика 2
+        picker2.doWork();
+        courier2.doWork();
+
+        System.out.println(picker1);
+        System.out.println(warehouse1);
+        System.out.println(courier1);
 
     }
+
+    private static void businessProcess(Worker worker) {
+        for(int i = 1; i<=10000; i++){
+            worker.doWork();
+        }
+        worker.bonus();
+    }
 }
+class Warehouse{
+    static int countPickedOrders;
+    static int countDeliveredOrders;
+    public String toString(){
+       return "Количество собранных заказов склада " + " = " + getCountPickedOrders() + ", " + "количество доставленных заказов " + getCountDeliveredOrders() + ".";
+    }
+
+    public static void setCountPickedOrders() {
+        countPickedOrders++;
+    }
+
+    public static void setCountDeliveredOrders() {
+        countDeliveredOrders++;
+    }
+
+    public static int getCountPickedOrders() {
+       return countPickedOrders;
+    }
+
+    public static int getCountDeliveredOrders() {
+        return countDeliveredOrders;
+    }
+}
+class Picker implements Worker{
+    private int salary;
+    private int isPayed;
+    private Warehouse warehouse;
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getIsPayed() {
+        return isPayed;
+    }
+    public Picker(Warehouse warehouse){
+        this.warehouse = warehouse;
+
+    }
+    public String toString(){
+        return "Заработная плата сборщика " + getSalary() + ", " + "был выплачен бонус или нет " + getIsPayed() + ".";
+    }
+
+    @Override
+    public void bonus() {
+        if(warehouse.getCountPickedOrders() >= 10000){
+            isPayed = isPayed + 70000;
+        } else {
+            System.out.println("Бонус пока не доступен");
+        }
+        if(isPayed > 0){
+            System.out.println("Бонус уже был выплачен");
+        }
+    }
+
+    @Override
+    public void doWork() {
+        salary = salary + 80;
+        warehouse.setCountPickedOrders(); //увеличение собранного заказа на 1
+        }
+}
+class Courier implements Worker{
+    private int salary;
+    private int isPayed;
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getIsPayed() {
+        return isPayed;
+    }
+    public Courier(Warehouse warehouse){
+
+    }
+    public String toString(){
+       return "Заработная плата курьера " + getSalary() + ", " + "был выплачен бонус или нет " + getIsPayed() + ".";
+    }
+
+    @Override
+    public void bonus() {
+        if(countDeliveredOrders >= 10000){
+            isPayed = isPayed + 50000;
+        } else {
+            System.out.println("Бонус пока не доступен");
+        }
+        if(isPayed > 0){
+            System.out.println("Бонус уже был выплачен");
+        }
+    }
+
+    @Override
+    public void doWork() {
+        salary = salary + 100;
+        setCountDeliveredOrders();
+        //System.out.println("Поработали, доставили товар");
+    }
+}
+
--- src/main/java/day11/task2/Task2.java
+++ src/main/java/day11/task2/Task2.java
@@ -2,6 +2,261 @@ package day11.task2;
 
 public class Task2 {
     public static void main(String[] args) {
+        Warrior warrior =  new Warrior();
+        Paladin paladin = new Paladin();
+        Magician magician = new Magician();
+        Shaman shaman = new Shaman();
 
+        warrior.physicalAttack(paladin); //1. Воин атакует Паладина
+        System.out.println(paladin);
+        paladin.physicalAttack(magician); //2. Паладин атакует Мага
+        System.out.println(magician);
+        shaman.healTeammate(magician); //3. Шаман лечит Мага
+        System.out.println(magician);
+        magician.magicalAttack(paladin); //4. Маг атакует Паладина, тип атаки М
+        System.out.println(paladin);
+        shaman.physicalAttack(warrior); //5. Шаман атакует Воина, тип атаки Ф
+        System.out.println(warrior);
+        paladin.healHimself(); //6. Паладин лечит себя
+        System.out.println(paladin);
+        System.out.println();
+        for (int i = 0; i<5; i++) { //7. Воин атакует Мага 5 раз
+            warrior.physicalAttack(magician);
+            System.out.println(magician);
+        }
+
+    }
+}
+abstract class Hero{
+    private int health;
+    private boolean physDef; //переменная типа атаки Ф - есть атака или нет
+    private boolean magicDef; //переменная тип атаки М - есть атака или нет
+
+    public abstract String toString();
+
+    public int getHealth(){
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public boolean isPhysDef() {
+        return physDef;
+    }
+
+    public void setPhysDef(boolean physDef) {
+        this.physDef = physDef;
+    }
+
+    public boolean isMagicDef() {
+        return magicDef;
+    }
+
+    public void setMagicDef(boolean magicDef) {
+        this.magicDef = magicDef;
+    }
+}
+class Warrior extends Hero implements PhysAttack{ //Воин
+    private int health = 100;
+    private boolean physDef = false; // изначально физической атаки нет
+    public int getHealth() {
+        return health;
+    }
+    public void setHealth(int health) {
+        if(this.physDef == true){ //срабатывает защита от физической атаки 80%
+            health = ((this.health - health)/10*8) + health;
+            this.physDef = false; //возвращаем тип
+        }
+        if (health > 100) { //Максимальное здоровье любого Героя - 100, минимальное - 0.
+            health = 100;
+        }
+        if (health < 0) {
+            health = 0;
+        }
+        this.health = health;
+    }
+
+    @Override
+    public void physicalAttack(Hero hero) { //физическая атака героя
+        hero.setPhysDef(true); //меняем значение физической атаки, чтобы сработала защита у героя, если она есть
+        hero.setHealth(hero.getHealth() - 30);
+    }
+    public String toString(){
+        return "Warrior{health=" + health + "}";
+    }
+
+    @Override
+    public boolean isPhysDef() {
+        return physDef;
+    }
+
+    @Override
+    public void setPhysDef(boolean physDef) {
+        this.physDef = physDef;
+    }
+}
+class Paladin extends Hero implements PhysAttack, Healer{ //Паладин
+    private int health = 100;
+    private boolean physDef = false; // изначально физической атаки нет
+    private boolean magicDef = false; // изначально магической атаки нет
+
+    public int getHealth() {
+        return health;
+    }
+    public void setHealth(int health) {
+       if(this.physDef == true){ //срабатывает защита от физической атаки 50% (1/2)
+           health = (this.health - health)/2 + health;
+           this.physDef = false;
+       }
+        if(this.magicDef == true){ //срабатывает защита от магической атаки 20% (атаку делим на 10 и умножаем на 8)
+            health = this.health - ((this.health - health)/10*8);
+            this.magicDef = false;
+        }
+        if (health > 100) {
+            health = 100;
+        }
+        if (health < 0) {
+            health = 0;
+        }
+        this.health = health;
+    }
+    @Override
+    public void physicalAttack(Hero hero) {
+        hero.setHealth(hero.getHealth() - 15);
+    }
+    public String toString(){
+        return "Paladin{health=" + health + "}";
+    }
+
+    @Override
+    public void healHimself() {
+        setHealth(getHealth() + 25);
+    }
+
+    @Override
+    public void healTeammate(Hero hero) {
+        hero.setHealth(hero.getHealth() + 10);
+    }
+
+    public boolean isPhysDef() {
+        return physDef;
+    }
+
+    public void setPhysDef(boolean physDef) {
+        this.physDef = physDef;
+    }
+
+    public boolean isMagicDef() {
+        return magicDef;
+    }
+
+    public void setMagicDef(boolean magicDef) {
+        this.magicDef = magicDef;
+    }
+}
+class Magician extends Hero implements MagicAttack{ //Маг
+    private int health = 100;
+    private boolean magicDef = false; // изначально магической атаки нет
+
+    public int getHealth() {
+        return health;
+    }
+    public void setHealth(int health) {
+        if(this.magicDef == true){ //срабатывает защита от физической атаки 80%
+            health = ((this.health - health)/10*8) + health;
+            this.magicDef = false; //возвращаем тип
+        }
+        if (health > 100) {
+            health = 100;
+        }
+        if (health < 0) {
+            health = 0;
+        }
+        this.health = health;
+    }
+    public String toString(){
+        return "Magician{health=" + health + "}";
+    }
+
+    @Override
+    public void magicalAttack(Hero hero) {
+        hero.setMagicDef(true); //меняем значение магической атаки, чтобы сработала защита у героя, если она есть
+        hero.setHealth(hero.getHealth() - 20);
+    }
+    public void physicalAttack(Hero hero) {
+        hero.setPhysDef(true); //меняем значение физической атаки, чтобы сработала защита у героя, если она есть
+        hero.setHealth(hero.getHealth() - 5);
+    }
+    public boolean isMagicDef() {
+        return magicDef;
+    }
+
+    public void setMagicDef(boolean magicDef) {
+        this.magicDef = magicDef;
+    }
+}
+class Shaman extends Hero implements Healer, PhysAttack{ //Шаман
+    private int health = 100;
+    private boolean physDef = false; // изначально физической атаки нет
+    private boolean magicDef = false; // изначально магической атаки нет
+    public int getHealth() {
+        return health;
+    }
+    public void setHealth(int health) {
+        if(this.physDef == true){ //срабатывает защита от магической атаки 20% (атаку делим на 10 и умножаем на 8)
+            health = this.health - ((this.health - health)/10*8);
+            this.physDef = false;
+        }
+        if(this.magicDef == true){ //срабатывает защита от магической атаки 20% (атаку делим на 10 и умножаем на 8)
+            health = this.health - ((this.health - health)/10*8);
+            this.magicDef = false;
+        }
+        if (health > 100) {
+            health = 100;
+        }
+        if (health < 0) {
+            health = 0;
+        }
+        this.health = health;
+    }
+    public String toString(){
+        return "Shaman{health=" + health + "}";
+    }
+
+    @Override
+    public void healHimself() {
+        setHealth(getHealth() + 50);
+    }
+
+    @Override
+    public void healTeammate(Hero hero) {
+        hero.setHealth(hero.getHealth() + 30);
+    }
+
+    @Override
+    public void physicalAttack(Hero hero) {
+        hero.setPhysDef(true); //меняем значение физической атаки, чтобы сработала защита у героя, если она есть
+        hero.setHealth(hero.getHealth() - 10);
+    }
+    public void magicalAttack(Hero hero) {
+        hero.setMagicDef(true); //меняем значение магической атаки, чтобы сработала защита у героя, если она есть
+        hero.setHealth(hero.getHealth() - 15);
+    }
+    public boolean isPhysDef() {
+        return physDef;
+    }
+
+    public void setPhysDef(boolean physDef) {
+        this.physDef = physDef;
+    }
+
+    public boolean isMagicDef() {
+        return magicDef;
+    }
+
+    public void setMagicDef(boolean magicDef) {
+        this.magicDef = magicDef;
     }
 }
