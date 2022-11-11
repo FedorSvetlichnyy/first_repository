3a79b89ae0c839fff83a7eb20beb570086e0b3c0
FedorSvetlichnyy <svfedor84@gmail.com>
2022-11-11 19:44:42 +0300

    complete day17 tasks

--- src/main/java/day18/Task3.java
+++ src/main/java/day18/Task3.java
@@ -1,7 +1,147 @@
 package day18;
 
+import java.util.*;
+
 public class Task3 {
     public static void main(String[] args) {
+        Node root = new Node();
+        root.setValue(20);
+        addNode(14, root);
+        addNode(11, root);
+        addNode(16, root);
+        addNode(5, root);
+        addNode(8, root);
+        addNode(15, root);
+        addNode(18, root);
+        addNode(23, root);
+        addNode(22, root);
+        addNode(27, root);
+        addNode(24, root);
+        addNode(150, root);
+        dfs(root);
 
+    }
+
+    public static void printTree(Node rootNode) { // метод для вывода дерева в консоль
+        Stack globalStack = new Stack(); // общий стек для значений дерева
+        globalStack.push(rootNode);
+        int gaps = 32; // начальное значение расстояния между элементами
+        boolean isRowEmpty = false;
+        String separator = "-----------------------------------------------------------------";
+        System.out.println(separator);// черта для указания начала нового дерева
+        while (isRowEmpty == false) {
+            Stack localStack = new Stack(); // локальный стек для задания потомков элемента
+            isRowEmpty = true;
+
+            for (int j = 0; j < gaps; j++)
+                System.out.print(' ');
+            while (globalStack.isEmpty() == false) { // покуда в общем стеке есть элементы
+                Node temp = (Node) globalStack.pop(); // берем следующий, при этом удаляя его из стека
+                if (temp != null) {
+                    System.out.print(temp.getValue()); // выводим его значение в консоли
+                    localStack.push(temp.getLeftChild()); // соохраняем в локальный стек, наследники текущего элемента
+                    localStack.push(temp.getRightChild());
+                    if (temp.getLeftChild() != null ||
+                            temp.getRightChild() != null)
+                        isRowEmpty = false;
+                }
+                else {
+                    System.out.print("__");// - если элемент пустой
+                    localStack.push(null);
+                    localStack.push(null);
+                }
+                for (int j = 0; j < gaps * 2 - 2; j++)
+                    System.out.print(' ');
+            }
+            System.out.println();
+            gaps /= 2;// при переходе на следующий уровень расстояние между элементами каждый раз уменьшается
+            while (localStack.isEmpty() == false)
+                globalStack.push(localStack.pop()); // перемещаем все элементы из локального стека в глобальный
+        }
+        System.out.println(separator);// подводим черту
+    }
+    public static ArrayList<Integer> array = new ArrayList<>();
+
+    public static void dfs(Node node){
+        dfsModified(node);
+        Collections.sort(array);
+        for(int counter: array){
+            System.out.print(counter + " ");
+        }
+    }
+
+    public static void dfsModified(Node node) {
+        if (node != null) {
+            array.add(node.getValue());
+            dfsModified(node.getLeftChild());
+            dfsModified(node.getRightChild());
+        }
+    }
+
+
+
+    public static void addNode(int value, Node rootNode) { // метод вставки нового элемента
+        Node newNode = new Node(); // создание нового узла
+        newNode.setValue(value); // вставка данных
+        if (rootNode == null) { // если корневой узел не существует
+            rootNode = newNode;// то новый элемент и есть корневой узел
+        } else { // корневой узел занят
+            Node currentNode = rootNode; // начинаем с корневого узла
+            Node parentNode;
+            while (true) // мы имеем внутренний выход из цикла
+            {
+                parentNode = currentNode;
+                if (value == currentNode.getValue()) {   // если такой элемент в дереве уже есть, не сохраняем его
+                    return;    // просто выходим из метода
+                } else if (value < currentNode.getValue()) {   // движение влево?
+                    currentNode = currentNode.getLeftChild();
+                    if (currentNode == null) { // если был достигнут конец цепочки,
+                        parentNode.setLeftChild(newNode); //  то вставить слева и выйти из методы
+                        return;
+                    }
+                } else { // Или направо?
+                    currentNode = currentNode.getRightChild();
+                    if (currentNode == null) { // если был достигнут конец цепочки,
+                        parentNode.setRightChild(newNode);  //то вставить справа
+                        return; // и выйти
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+class Node {
+    private int value; // ключ узла
+    private Node leftChild; // Левый узел потомок
+    private Node rightChild; // Правый узел потомок
+
+    public void printNode() { // Вывод значения узла в консоль
+        System.out.println("Выбранный узел имеет значение: " + value);
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public void setValue(final int value) {
+        this.value = value;
+    }
+
+    public Node getLeftChild() {
+        return this.leftChild;
+    }
+
+    public void setLeftChild(final Node leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public Node getRightChild() {
+        return this.rightChild;
+    }
+
+    public void setRightChild(final Node rightChild) {
+        this.rightChild = rightChild;
     }
 }
