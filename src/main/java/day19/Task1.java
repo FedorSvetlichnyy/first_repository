eb463dddddff2276eb6d31db27ad08f3a511fe4b
FedorSvetlichnyy <svfedor84@gmail.com>
2022-11-18 20:49:51 +0300

    complete day19 tasks

--- src/main/java/day19/Task1.java
+++ src/main/java/day19/Task1.java
@@ -1,10 +1,63 @@
 package day19;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 /**
  * @author Neil Alishev
  */
 public class Task1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        // „ичиков - 601 (Chichikov)
 
+        File textError = new File("src\\main\\resources\\dushi.txt"); //кодировка (encoding)
+        File text = new File("test19.txt"); //copy of dushi.txt
+
+
+        Scanner scanner = new Scanner(text);
+        scanner.useDelimiter("[.,:;()?!\"\\sЦ]+");
+
+        List<String> list = new ArrayList();
+        while (scanner.hasNext())
+            list.add(scanner.next());
+
+        String[] words = list.toArray(new String[0]);
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        int count = 0;
+        for (int i = 0; i < words.length; i++) {
+            count = 0;
+            for (int j = 0; j < words.length; j++) {
+                if (words[i].equals(words[j])) {
+                    count++;
+                }
+            }
+            map.put(words[i], count);
+        }
+
+        //can be displayed on the screen "Chichikov"
+        System.out.println(map.get("„ичиков") + "  раз в произведении было использовано слово У„ичиковФ (фамили€ главного геро€)."); // „ичиков - 601
+        //System.out.println(entriesSortedByValues(map));
+
+        Object[] words100 = entriesSortedByValues(map).toArray();
+        for (int i = 0; i < 100; i++) {
+            System.out.println(words100[i]);
+        }
+    }
+
+
+    static <K, V extends Comparable<? super V>>
+    SortedSet<Map.Entry<K, V>> entriesSortedByValues(Map<K, V> map) {
+        SortedSet<Map.Entry<K, V>> sortedEntries = new TreeSet<Map.Entry<K, V>>(
+                new Comparator<Map.Entry<K, V>>() {
+                    @Override
+                    public int compare(Map.Entry<K, V> e1, Map.Entry<K, V> e2) {
+                        int res = e2.getValue().compareTo(e1.getValue());
+                        return res != 0 ? res : 1;
+                    }
+                }
+        );
+        sortedEntries.addAll(map.entrySet());
+        return sortedEntries;
     }
 }
--- src/main/java/day19/task2/Task2.java
+++ src/main/java/day19/task2/Task2.java
@@ -1,11 +1,116 @@
 package day19.task2;
 
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * @author Neil Alishev
  */
 public class Task2 {
     public static void main(String[] args) {
 
+        Map<Integer, Point> map = getHashMapFromTextFile();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите координаты квадрата (x1, y1, x2, y2): ");
+
+        int x1;
+        do {
+            System.out.println("Введите координаты точки x1 от 0 до 99");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Это не число!");
+                scanner.next();
+            }
+            x1 = scanner.nextInt();
+        } while (x1 < 0 | x1 > 99);
+
+        int y1;
+        do {
+            System.out.println("Введите координаты точки y1 от 0 до 99");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Это не число!");
+                scanner.next();
+            }
+            y1 = scanner.nextInt();
+        } while (y1 < 0 | y1 > 99);
+
+        int x2;
+        do {
+            System.out.println("Введите координаты точки x2 от 0 до 99");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Это не число!");
+                scanner.next();
+            }
+            x2 = scanner.nextInt();
+        } while (x2 < 0 | x2 > 99);
+
+        int y2;
+        do {
+            System.out.println("Введите координаты точки y2 от 0 до 99");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Это не число!");
+                scanner.next();
+            }
+            y2 = scanner.nextInt();
+        } while (y2 < 0 | y2 > 99);
+
+        scanner.close();
+
+        int carsVolume = 0;
+        for (Map.Entry<Integer, Point> enrty : map.entrySet()) {
+            if (x1 < enrty.getValue().getX() & x2 > enrty.getValue().getX() & y1 > enrty.getValue().getY() & y2 < enrty.getValue().getY()) {
+                carsVolume++;
+                System.out.println("В заданном квадрате машина с идентификатором: " + enrty.getKey());
+            }
+            if (x1 > enrty.getValue().getX() & x2 < enrty.getValue().getX() & y1 < enrty.getValue().getY() & y2 > enrty.getValue().getY()) {
+                carsVolume++;
+                System.out.println("В заданном квадрате машина с идентификатором: " + enrty.getKey());
+            }
+            if (x1 < enrty.getValue().getX() & x2 > enrty.getValue().getX() & y1 < enrty.getValue().getY() & y2 > enrty.getValue().getY()) {
+                carsVolume++;
+                System.out.println("В заданном квадрате машина с идентификатором: " + enrty.getKey());
+            }
+            if (x1 > enrty.getValue().getX() & x2 < enrty.getValue().getX() & y1 > enrty.getValue().getY() & y2 < enrty.getValue().getY()) {
+                carsVolume++;
+                System.out.println("В заданном квадрате машина с идентификатором: " + enrty.getKey());
+            }
+        }
+        System.out.println("В заданном квадрате " + carsVolume + " машин(-а, -ы).");
+
+    }
+
+    public static Map<Integer, Point> getHashMapFromTextFile() {
+
+        Map<Integer, Point> mapFileContents = new HashMap<Integer, Point>();
+        BufferedReader br = null;
+
+        try {
+            File file = new File("src\\main\\resources\\taxi_cars.txt");
+
+            br = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = br.readLine()) != null) {
+
+                String[] id_x_y = line.split(" ");
+                Integer key = Integer.parseInt(id_x_y[0].trim());
+                Integer x = Integer.parseInt(id_x_y[1].trim());
+                Integer y = Integer.parseInt(id_x_y[2].trim());
+                Point point = new Point(x, y);
+                mapFileContents.put(key, point);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (Exception e) {
+                }
+            }
+        }
+
+        return mapFileContents;
     }
 }
-
